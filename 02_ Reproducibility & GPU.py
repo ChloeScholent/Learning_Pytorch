1a33632427295a226b6b54c07d8811645894952d
ChloeScholent <159830563+ChloeScholent@users.noreply.github.com>
2025-08-12 23:30:45 +0200

    Update 02_ Reproducibility & GPU.py

--- 02_ Reproducibility & GPU.py
+++ 02_ Reproducibility & GPU.py
@@ -21,28 +21,20 @@ random_tensor_D = torch.rand(4,5)
 
 print(random_tensor_C == random_tensor_D)
 
-
 #torch.manual_seed(RANDOM_SEED) needs to be added everytime a new tensor is created, otherwise it will not work.abs
 #This allows different randdoms seeds to be used
 
-
-
 #Check if GPU is available for Pytorch
-
 print(torch.cuda.is_available())
 
 #Setup device agnostic code
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 #Count nb of gpu
 print(torch.cuda.device_count())
 
-
 #Putting tensors and models on GPU
-
 tensor_CPU = torch.rand(5,7)
-
 print(tensor_CPU.device)
 
 #Move to GPU
@@ -50,6 +42,5 @@ tensor_to_GPU = tensor_CPU.to(device)
 print(tensor_to_GPU)
 
 #Move back to CPU (for Numpy for instance)
-
 tensor_back_to_CPU = tensor_to_GPU.cpu()
 print(tensor_back_to_CPU.device)
